429e63f41bd74335ec77790a080c8ea64c681cca
arma7x <arma7x@live.com>
2021-04-24 18:55:38 +0800

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2347,6 +2347,20 @@ window.addEventListener("load", function() {
   });
 
   function displayKaiAds() {
+    var display = true;
+    if (window['kaiadstimer'] == null) {
+      window['kaiadstimer'] = new Date();
+    } else {
+      var now = new Date();
+      if ((now - window['kaiadstimer']) < 300000) {
+        display = false;
+      } else {
+        window['kaiadstimer'] = now;
+      }
+    }
+    console.log('Display Ads:', display);
+    if (!display)
+      return;
     getKaiAd({
       publisher: 'ac3140f7-08d6-46d9-aa6f-d861720fba66',
       app: 'k-music',
